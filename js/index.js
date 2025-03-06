0981756c2a8ba5aa904140b3dc3369eb31a4aa89
rifathassan478 <rifathassanrana@gmail.com>
2025-03-06 23:48:49 +0600

    Formatted JS code

--- js/index.js
+++ js/index.js
@@ -1,58 +1,60 @@
-document
-  .getElementById("discover-something")
-  .addEventListener("click", function () {
-    window.location.href = "blog.html";
-  });
+document.getElementById('discover-something').onclick = function () {
+  window.location.href = 'blog.html';
+};
 
 let now = new Date();
 let options = {
-  weekday: "short",
-  month: "short",
-  day: "2-digit",
-  year: "numeric",
+  weekday: 'short',
+  month: 'short',
+  day: '2-digit',
+  year: 'numeric',
 };
-let formattedDate = now.toLocaleDateString("en-US", options).replace(",", "");
-const dynamicTime = document.getElementById("dynamic-time");
+let formattedDate = now.toLocaleDateString('en-US', options).replace(',', '');
+const dynamicTime = document.getElementById('dynamic-time');
 dynamicTime.innerText = `${formattedDate}`;
 
-const completedButtons = document.querySelectorAll(".completed-btn");
+
+const completedButtons = document.querySelectorAll('.completed-btn');
 for (const completedButton of completedButtons) {
-  completedButton.addEventListener("click", function (event) {
+  completedButton.addEventListener('click', function (event) {
     console.log(completedButton);
     const title =
-      event.target.parentNode.parentNode.getElementsByTagName("h3")[0]
+      event.target.parentNode.parentNode.getElementsByTagName('h3')[0]
         .innerHTML;
     console.log(title);
     console.log(completedButtons.length);
+
 
     // time start
     let now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
-    let amPm = hours >= 12 ? "PM" : "AM";
+    let amPm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12 || 12;
-    let formattedTime = `${hours}:${String(minutes).padStart(2, "0")}:${String(
+    let formattedTime = `${hours}:${String(minutes).padStart(2, '0')}:${String(
       seconds
-    ).padStart(2, "0")} ${amPm}`;
+    ).padStart(2, '0')} ${amPm}`;
     console.log(formattedTime);
     // time end
 
-    const historyList = document.getElementById("history-list");
-    const div = document.createElement("div");
+    const historyList = document.getElementById('history-list');
+    const div = document.createElement('div');
     div.innerHTML = `<div class="text-center my-5">
             <h3>You have Completed the task ${title} at ${formattedTime}</h3>
           </div>`;
     historyList.appendChild(div);
+
+    
     // Task Assigned
-    const assgintedTask = document.getElementById("assigned-task");
+    const assgintedTask = document.getElementById('assigned-task');
     const currentAssgintedTask = parseInt(assgintedTask.innerText);
     const newAssgintedTask = currentAssgintedTask - 1;
     console.log(newAssgintedTask);
     assgintedTask.innerText = newAssgintedTask;
 
     // DevBoard Task
-    const devBoardTask = document.getElementById("dev-board-task");
+    const devBoardTask = document.getElementById('dev-board-task');
     const currentDevBoardTask = parseInt(devBoardTask.innerText);
     const newDevBoardTask = currentDevBoardTask + 1;
     console.log(newDevBoardTask);
@@ -61,29 +63,29 @@ for (const completedButton of completedButtons) {
       console.log(currentAssgintedTask);
 
       if (currentAssgintedTask === 1) {
-        alert("Board Updated Successfully");
-        alert("Congrates!! You have Completed All the Current Task ");
-        completedButton.classList.add("bg-[#9ba8f8]");
+        alert('Board Updated Successfully');
+        alert('Congrates!! You have Completed All the Current Task ');
+        completedButton.classList.add('bg-[#9ba8f8]');
         completedButton.disabled = true;
       } else {
-        alert("Board Updated Successfully");
-        completedButton.classList.add("bg-[#9ba8f8]");
+        alert('Board Updated Successfully');
+        completedButton.classList.add('bg-[#9ba8f8]');
         completedButton.disabled = true;
       }
     }
   });
 }
 document
-  .getElementById("clear-history-btn")
-  .addEventListener("click", function () {
-    const historyList = document.getElementById("history-list");
-    historyList.innerText = "";
+  .getElementById('clear-history-btn')
+  .addEventListener('click', function () {
+    const historyList = document.getElementById('history-list');
+    historyList.innerText = '';
   });
 
 document
-  .getElementById("bg-theme-change")
-  .addEventListener("click", function () {
-    console.log("theme");
-    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  .getElementById('bg-theme-change')
+  .addEventListener('click', function () {
+    console.log('theme');
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
   });
